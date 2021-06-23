127f28613b5c0d8cd334ca39d6304d7d27b72c17
KHW24 <rlagpdnjs1266@gmail.com>
2021-06-23 17:10:32 +0900

    Adding todo

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo){
+    //console.log("i will paint", newTodo, newTodo);
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSumit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
-    
+    paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSumit);
