4f145ce04245acbb20de03d989d786fa2e707e81
KHW24 <rlagpdnjs1266@gmail.com>
2021-06-28 18:17:34 +0900

    Loadint To Dos part One

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,12 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos"
+
 const toDos = [];
 
 function saveToDos(){
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem("TODOS_KEY", JSON.stringify(toDos));
 }
 
 function deleteToDo(event){
@@ -38,3 +40,14 @@ function handleToDoSumit(event){
 }
 
 toDoForm.addEventListener("submit", handleToDoSumit);
+
+function sayHello(item){
+    console.log("this is the turn of", item);
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if(saveToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log("this is the turn of", item));
+}
