790df49d7cc06a6e145d65eec3fe0b4bec01dde5
KHW24 <rlagpdnjs1266@gmail.com>
2021-06-30 14:29:38 +0900

    배경화면 수정 중

--- js/background.js
+++ js/background.js
@@ -3,12 +3,11 @@ const images = ["0.jpeg", "1.jpg", "2.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
-
+// bgImage.id = "backGround";
 // html img 태그를 생성
 bgImage.src = `img/${chosenImage}`;
 
 // html에 body에 위에서 생성한 img 태그를 추가
 // append 가장 뒤에, prepend 가장 위에 위치시킴
 document.body.appendChild(bgImage);
-
 
